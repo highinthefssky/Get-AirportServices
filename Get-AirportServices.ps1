9550f66f75275e50cf5e355ae09ca47a367fa711
highinthefssky <70626202+highinthefssky@users.noreply.github.com>
2021-08-24 20:01:35 +0200

    Add files via upload

---
+++ Get-AirportServices.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis
+   Script to find AirportServices section in JSON file of mod
+.DESCRIPTION
+   Script to find AirportServices section in JSON file which could prevent MSFS 2020 groundservices to be unavailable
+.EXAMPLE
+   .\Get-AirportServices.ps1 -CommunityFolder C:\users\johan\appdata\local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community
+#>
+[CmdletBinding()]
+Param(
+[Parameter (Mandatory=$true)]
+$CommunityFolder = ""
+)
+
+$jsonFiles = @()
+$jsonFiles = Get-ChildItem $CommunityFolder -Filter layout.json -Recurse -Depth 1
+
+foreach ($jsonFile in $jsonFiles)
+{
+    $content = ""
+    $content = Get-Content $jsonFile.FullName -Raw|ConvertFrom-Json
+    $airportServicesFound = $false
+
+    foreach ($item in $($content.content.path))
+    {
+        if($item -like "*AirportServices*")
+        {
+                $AirportServicesFound = $true
+        }
+    }
+
+    if ($airportServicesFound)
+    {
+        Write-Host "AirportServices section found in $($jsonFile.FullName)"
+    }
+}
